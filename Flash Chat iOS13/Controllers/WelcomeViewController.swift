416343aa4815cfef06a7bf5ee8a58cf189072ec2
Ethan Smith <edhsmith@icloud.com>
2021-03-02 16:29:31 +1300

    Animated FlashChat intro logo.

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    
+    let finalTitle = "FlashChat"
+    var charIndex = 0.0
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = "⚡️"
+        
+        for letter in finalTitle {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
     
-
 }
