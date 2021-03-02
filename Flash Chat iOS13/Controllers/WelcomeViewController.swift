0a07eefbc3a708ad63b368ab53bda0f88ba076bd
Ethan Smith <edhsmith@icloud.com>
2021-03-02 21:51:26 +1300

    Added FireBase and updated CLTypingLabel.

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,25 +7,17 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
-    
-    let finalTitle = "FlashChat"
-    var charIndex = 0.0
-    
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = "⚡️"
         
-        for letter in finalTitle {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
+        
     }
     
 }
