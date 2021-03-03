da001c2967ad324b3dbb3477128e1ab318d0271d
Ethan Smith <edhsmith@icloud.com>
2021-03-03 16:05:56 +1300

    Added FireBase registration. Updated registration screen UI to reflect errors or success.

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import UIKit
 import Firebase
 
 @UIApplicationMain
@@ -17,14 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions:
-                        [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+                        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        return true
-    }
-    
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
     
@@ -33,9 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSessi
-                                    workspace
-                                    ethansmith@Ethans-MacBook-Proon.role)
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { [self] authResult, error in                if let e = error {
+                print(e.localizedDescription)
+                errorLabel.text = e.localizedDescription
+                errorLabel.isHidden = false
+            } else {
+                // Navigate to chat view controller
+                errorLabel.textColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+                errorLabel.text = "Sucess! You're now registered."
+                errorLabel.isHidden = false
+                performSegue(withIdentifier: "RegisterToChat", sender: self)
+            }
+            }
+        }
     }
     
 }
