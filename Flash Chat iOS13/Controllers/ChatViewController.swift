654fee45969ef24a4403f080d5f4825907fa24e9
Ethan Smith <edhsmith@icloud.com>
2021-03-05 19:49:18 +1300

    Added message layout for TableView. Updated TableView layout.

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,20 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey"),
+        Message(sender: "a@b.com", body: "Hello"),
+        Message(sender: "1@2.com", body: "What's up?"),
+        Message(sender: "1@2.com", body: "What shall we do with a drunken sailor? What shall we do with a drunken sailor? What shall we do with a drunken sailor, early in the morning?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         title = K.appName
         navigationItem.hidesBackButton = true
+
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -31,6 +40,21 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print ("Error signing out: %@", signOutError)
         }
+    }
+    
+}
+
+//MARK: - UITableViewDataSource
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
     }
     
 }
